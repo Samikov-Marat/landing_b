d7ec3497362e628f97f744e0b88f4624072beceb
Самиков Марат Рустамович <m.samikov@cdek.ru>
2020-10-29 14:00:14 +0700

    Композер. Доработка инициализации.

---
+++ docker/php/composer_setup_script.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+EXPECTED_CHECKSUM="$(php -r "readfile('https://composer.github.io/installer.sig');" )"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php  --version=1.10.16 --quiet
+RESULT=$?
+rm composer-setup.php
+mv composer.phar /usr/local/bin/composer
+
+echo 'composer_setup_script.sh finished'
+
+exit $RESULT
