7faf6249dc742544ef5d6316b66d03ceb798a177
Самиков Марат Рустамович <m.samikov@cdek.ru>
2020-10-29 19:46:11 +0700

    Тема для bootstrap 4 добавлена

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Schema\Builder;
 
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,10 +27,20 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Dispatcher $events)
     {
         Builder::defaultStringLength(191);
 
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('Администрирование');
+            $menu = [
+                ['route' => 'admin.sites.index', 'text' => 'Сайты', 'icon' => 'far fa-folder',],
+                ['route' => 'admin.pages.index', 'text' => 'Страницы', 'icon' => 'far fa-file',],
+            ];
+            foreach ($menu as $item){
+                $event->menu->add($item);
+            }
+        });
 
         Blade::directive('d', function ($expression) {
             return "<?php echo nl2br(e(\$dictionary[$expression])); ?>";
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,54 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('adminlte::page')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+@section('title', 'Dashboard')
 
-    <title>Редактирование лендингов</title>
-</head>
-<body>
+@section('content_header')
+    @yield('buttons')
+@stop
 
-<div class="container">
-    <div class="row">
-        <div class="col">
-            @include('admin.menu')
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
-        </div>
-        <div class="col-auto">
-            @auth
-                <form method="post" action="{!! route('logout') !!}">
-                    @csrf
-                    <button type="submit" class="btn btn-secondary">Выход</button>
-                </form>
-            @endauth
-        </div>
-    </div>
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
 
-    <div class="row mt-5">
-        <div class="col">
-            @yield('buttons')
-            @yield('content')
-        </div>
-    </div>
-</div>
+@section('js')
+    <script src="/admin_files/admin.js"></script>
+    <script src="/admin_files/texts.js"></script>
+@stop
 
-@include('admin.delete_confirm')
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
-<script src="/admin_files/admin.js"></script>
-<script src="/admin_files/texts.js"></script>
-</body>
-</html>
+@section('footer')
+    @include('admin.delete_confirm')
+@stop
