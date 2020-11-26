93c4285d55cc585a01577a6ebde6c41f93c74b1f
Самиков Марат Рустамович <m.samikov@cdek.ru>
2020-11-26 16:12:34 +0700

    Revert "Разворачивание системы разрешений. Отключил проверку Gate"
    This reverts commit 0c5b7846d007ce466bdcd6c7994cade8733233c7.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,7 +31,6 @@ class AuthServiceProvider extends ServiceProvider
             Gate::define(
                 $permission->text_id,
                 function ($user) use ($permission) {
-                    return true;
                     return $user->permissions->contains('text_id', $permission->text_id);
                 }
             );
