0b405e99a4a9a050277d9a573c6a84340b84d6ac
Марат Самиков <m.samikov@cdek.ru>
2021-04-12 15:24:31 +0700

    Блок Два способа получить посылку на странице Корпоративных клиентов. Заменил статику на переменные

--- resources/views/site/universal2/business.blade.php
+++ resources/views/site/universal2/business.blade.php
@@ -76,24 +76,24 @@
         <div class="package-russia screen">
     <div class="package-russia__content content">
         <div class="package-russia__text">
-            <h2 class="typo-h2 package-russia__title">Два способа получить посылку в России</h2>
+            <h2 class="typo-h2 package-russia__title">@d('receive_title')</h2>
             <div class="package-russia__ways">
                 <div class="package-russia__way">
                     <div class="circle-icon circle-icon_icon_map"></div>
-                    <div class="package-russia__way-text">Доставка в пункт выдачи заказов</div>
+                    <div class="package-russia__way-text">@d('receive_text_1')</div>
                 </div>
                 <div class="package-russia__way">
                     <div class="circle-icon circle-icon_icon_hand-box"></div>
-                    <div class="package-russia__way-text">Курьерская доставка от двери до двери</div>
+                    <div class="package-russia__way-text">@d('receive_text_2')</div>
                 </div>
             </div>
             <div class="divider package-russia__divider"></div>
-            <div class="package-russia__know-more">Узнайте больше о сотрудничестве со СДЭК!</div>
-            <a href="#" class="primary-button js-feedback-open">Свяжитесь с нами</a>
+            <div class="package-russia__know-more">@d('receive_more')</div>
+            <a href="#" class="primary-button js-feedback-open">@d('receive_button')</a>
         </div>
         <div class="package-russia__office">
-            <div class="package-russia__office-num">2 300+</div>
-            <div class="package-russia__office-text">Офисов в 20 странах</div>
+            <div class="package-russia__office-num">@d('receive_office_count')</div>
+            <div class="package-russia__office-text">@d('receive_country_count')</div>
         </div>
     </div>
 </div>
