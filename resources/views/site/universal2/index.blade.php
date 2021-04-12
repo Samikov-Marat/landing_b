1d68beabe5ad251192eeaf4bbd7975be32bc6602
Марат Самиков <m.samikov@cdek.ru>
2021-04-12 16:27:32 +0700

    Срочно

--- resources/views/site/universal2/index.blade.php
+++ resources/views/site/universal2/index.blade.php
@@ -89,30 +89,7 @@
 
         @include('site.universal2.how_it_works_block')
 
-        <div class="package-russia screen">
-    <div class="package-russia__content content">
-        <div class="package-russia__text">
-            <h2 class="typo-h2 package-russia__title">@d('receive_title')</h2>
-            <div class="package-russia__ways">
-                <div class="package-russia__way">
-                    <div class="circle-icon circle-icon_icon_map"></div>
-                    <div class="package-russia__way-text">@d('receive_text_1')</div>
-                </div>
-                <div class="package-russia__way">
-                    <div class="circle-icon circle-icon_icon_hand-box"></div>
-                    <div class="package-russia__way-text">@d('receive_text_2')</div>
-                </div>
-            </div>
-            <div class="divider package-russia__divider"></div>
-            <div class="package-russia__know-more">@d('receive_more')</div>
-            <a href="#" class="primary-button js-feedback-open">@d('receive_button')</a>
-        </div>
-        <div class="package-russia__office">
-            <div class="package-russia__office-num">@d('receive_office_count')</div>
-            <div class="package-russia__office-text">@d('receive_country_count')</div>
-        </div>
-    </div>
-</div>
+        @include('site.universal2.package_russia_block')
 
         @include('site.universal2.faq_block')
 
