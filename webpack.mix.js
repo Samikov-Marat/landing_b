e14fb095f87fe6dfac318555d61ad7e8f6b8af20
Марат Самиков <m.samikov@cdek.ru>
2021-07-30 12:38:05 +0700

    js калькулятора вынес в отдельный файл

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,12 +4,22 @@ mix.combine([
     'public/universal2/assets/vendor/jquery.min.js',
     'public/universal2/assets/vendor/owl.carousel.min.js',
     'public/universal2/assets/vendor/jquery.autocomplete.js',
-    'resources/js/universal2/tariffs.js',
     'resources/js/universal2/app.js',
+    'resources/js/universal2/tariffs.js',
+    'resources/js/universal2/calculator.js',
     'resources/js/universal2/faq.js',
     'resources/js/universal2/how_it_works.js',
     'resources/js/universal2/map.js'
 ], 'public/universal2/new.js').version();
+
+mix.combine([
+    'public/personal/assets/vendor/jquery.min.js',
+    'public/personal/assets/vendor/owl.carousel.min.js',
+    'public/personal/assets/vendor/jquery.autocomplete.js',
+    'resources/js/personal/app.js',
+    'resources/js/universal2/tariffs.js',
+    'resources/js/universal2/calculator.js',
+], 'public/personal/new.js').version();
 
 
 mix.copy('resources/css/universal2/app.css', 'public/universal2/app.css').version();
