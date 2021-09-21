f7ed8abd6872353e03cfeec2cf8f253ebe15ad77
Марат Самиков <m.samikov@cdek.ru>
2021-09-21 20:55:22 +0700

    MRKT-404 Мелкие правки

--- app/Classes/TextTypesCleaner.php
+++ app/Classes/TextTypesCleaner.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Classes;
+
 use App\TextType;
 
 use Illuminate\Support\Facades\Storage;
@@ -8,37 +9,32 @@ use Illuminate\Support\Str;
 
 class TextTypesCleaner
 {
-
-    public function clear(){
+    public function clear()
+    {
         $files = Storage::disk('views')->allFiles('site');
 
         $usedTextTypes = collect();
 
-        foreach ($files as $file){
+        foreach ($files as $file) {
             $contents = Storage::disk('views')->get($file);
             preg_match_all('#\\$dictionary\\[\'(.*?)\'\\]#u', $contents, $m);
-            foreach ($m[1] as $shortname){
+            foreach ($m[1] as $shortname) {
                 $usedTextTypes->push($shortname);
             }
             preg_match_all('#@d\\(\'(.*?)\'\\)#u', $contents, $m);
-            foreach ($m[1] as $shortname){
+            foreach ($m[1] as $shortname) {
                 $usedTextTypes->push($shortname);
             }
         }
 
         $textTypes = TextType::select('id', 'shortname')->get();
-        foreach ($textTypes as $textType){
-            if(Str::startsWith($textType->shortname, '_')){
-                dump('>>> ' . $textType->shortname);
+        foreach ($textTypes as $textType) {
+            if (Str::startsWith($textType->shortname, '_')) {
                 continue;
             }
-
-            if(!$usedTextTypes->contains($textType->shortname)){
-                dump($textType->shortname);
+            if (!$usedTextTypes->contains($textType->shortname)) {
+                $textType->delete();
             }
         }
-
     }
-
-
 }
