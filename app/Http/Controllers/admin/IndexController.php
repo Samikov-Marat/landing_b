782116acfbc11f6a728e1fe27d52121e36774199
Марат Самиков <m.samikov@cdek.ru>
2021-10-14 21:01:23 +0700

    MRKT-443 Проверка наличия офисов и категорий

--- app/Http/Controllers/admin/IndexController.php
+++ app/Http/Controllers/admin/IndexController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\LocalOffice;
+use App\Site;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $columns = [
+            'innerKey' => LocalOffice::newModelInstance()->qualifyColumn('site_id'),
+            'outterKey' => Site::newModelInstance()->qualifyColumn('id'),
+            'category' => LocalOffice::newModelInstance()->qualifyColumn('category'),
+        ];
+
+        $localOfficeExistsQuery = LocalOffice::selectRaw('1')
+            ->whereColumn($columns['innerKey'], '=', $columns['outterKey'])
+            ->getQuery();
+
+        $localOfficeHaveEmptyCategoryQuery = LocalOffice::selectRaw('1')
+            ->whereColumn($columns['innerKey'], '=', $columns['outterKey'])
+            ->where($columns['category'], '')
+            ->getQuery();
+
+        $problems = [];
+
+        $problems['absentOffice'] = Site::select(['id', 'name', 'domain'])
+            ->addWhereExistsQuery($localOfficeExistsQuery, ' and', true)
+            ->get();
+
+        $problems['absentOfficeCategory'] = Site::select(['id', 'name', 'domain'])
+            ->addWhereExistsQuery($localOfficeHaveEmptyCategoryQuery)
+            ->with([
+                       'localOffices' => function ($q) {
+                           $q->where('category', '');
+                       }
+                   ])
+            ->get();
+
+
+        return view('admin.index')
+            ->with('problems', $problems);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,35 @@
 
 @section('content')
 
-Главная страница админки.
+    Главная страница админки.
+
+    @foreach($problems['absentOffice'] as $site)
+        <div class="alert alert-danger">
+            <h4 class="alert-heading">На сайте нет ни одного офиса</h4>
+            <p>
+                {{ $site->domain }} {{ $site->name }}
+            </p>
+            <hr>
+            <p class="mb-0">
+                При заполнении и отправке форм произойдёт ошибка
+            </p>
+        </div>
+    @endforeach
+
+    @foreach($problems['absentOfficeCategory'] as $site)
+        @foreach($site->localOffices as $office)
+            <div class="alert alert-danger">
+                <h4 class="alert-heading">В одном из офисов не указана категория</h4>
+                <p>
+                    {{ $site->domain }} {{ $site->name }}<br>
+                    Офисе {{ $office->code }}
+                </p>
+                <hr>
+                <p class="mb-0">
+                    При заполнении и отправке форм произойдёт ошибка
+                </p>
+            </div>
+        @endforeach
+    @endforeach
+
 @endsection
