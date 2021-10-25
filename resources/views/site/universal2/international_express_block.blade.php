f0249cc0a2557480d0b8d65d4c6040babac5021b
Марат Самиков <m.samikov@cdek.ru>
2021-10-25 18:55:06 +0700

    MRKT-453 Блок тарифа отдельно

---
+++ resources/views/site/universal2/international_express_block.blade.php
@@ -0,0 +1,46 @@
+<div class="company-advantages screen-content">
+    <h2 class="typo-h2 company-advantages__heading business-page__company-adv-heading">@d('advantages_header_1') <span class="typo-colored_color_green">@d('advantages_header_2')</span></h2>
+    <div class="company-advantages__description business-page__company-adv-description">
+        @d('advantages_text')
+    </div>
+    <div class="two-icons business-page__company-adv-icons">
+        <div class="two-icons__item">
+            <div class="two-icons__icon"></div>
+            <div class="two-icons__text"><span class="two-icons__text-big">@d('1_weight') @d('advantages_kg')</span> @d('advantages_from') <span class="two-icons__text-big">@d('1_weight_price')</span></div>
+        </div>
+        <div class="two-icons__item">
+            <div class="two-icons__icon two-icons__icon_icon_second"></div>
+            <div class="two-icons__text"><span class="two-icons__text-big">@d('2_weight') @d('advantages_kg')</span> @d('advantages_from') <span class="two-icons__text-big">@d('2_weight_price')</span></div>
+        </div>
+    </div>
+    <div class="square-cards js-company-advantages owl-carousel">
+        <div class="square-card square-card_size_big square-card_big-icon_person">
+            <div class="square-card__bg"></div>
+            <div class="square-card__title">@d('carousel_header_1')</div>
+            <div class="square-card__description">
+                @d('carousel_text_1')
+            </div>
+        </div>
+        <div class="square-card square-card_size_big square-card_big-icon_contract">
+            <div class="square-card__bg"></div>
+            <div class="square-card__title">@d('carousel_header_2')</div>
+            <div class="square-card__description">
+                @d('carousel_text_2')
+            </div>
+        </div>
+        <div class="square-card square-card_size_big square-card_big-icon_mail">
+            <div class="square-card__bg"></div>
+            <div class="square-card__title">@d('carousel_header_3')</div>
+            <div class="square-card__description">
+                @d('carousel_text_3')
+            </div>
+        </div>
+        <div class="square-card square-card_size_big square-card_big-icon_courier">
+            <div class="square-card__bg"></div>
+            <div class="square-card__title">@d('carousel_header_4')</div>
+            <div class="square-card__description">
+                @d('carousel_text_4')
+            </div>
+        </div>
+    </div>
+</div>
