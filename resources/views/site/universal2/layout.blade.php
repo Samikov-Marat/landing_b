3c34c36466dae4a6cd68d2ff8df41d0a0602e344
Марат Самиков <m.samikov@cdek.ru>
2021-12-23 19:08:05 +0700

    MRKT-473 Политика конфиденциальности и юридическая информация. Доработка

--- resources/views/site/universal2/layout.blade.php
+++ resources/views/site/universal2/layout.blade.php
@@ -126,13 +126,12 @@
 
                 <div class="footer__link-policy-item">
                     <a class="footer__link footer__link-mr-policy" href="{!! route('site.show_page', ['languageUrl' => $language->uri, 'pageUrl' => 'contacts']) !!}">@d('menu_contects')</a>
-
-                    @if(isset($dictionary['privacy_policy_content']))
+                    @if($dictionary['footer_has_privacy_policy_page'] != '-')
                         <a class="footer__link footer__link-mr-policy" href="{!! route('site.show_page', ['languageUrl' => $language->uri, 'pageUrl' => 'privacy-policy']) !!}">@d('footer_policy')</a>
                     @else
                         <a class="footer__link footer__link-mr-policy" target="_blank" href="/storage/Privacy_Policy.pdf">@d('footer_policy')</a>
                     @endif
-                    @if(isset($dictionary['legal_content']))
+                    @if($dictionary['footer_has_legal_page'] != '-')
                         <a class="footer__link footer__link-mr-policy" href="{!! route('site.show_page', ['languageUrl' => $language->uri, 'pageUrl' => 'impressum']) !!}">@d('footer_legal_info')</a>
                     @endif
                 </div>
