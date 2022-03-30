f6b3b3c25665f79f63539458f161be9449acc4c0
Марат Самиков <m.samikov@cdek.ru>
2022-03-30 15:54:17 +0700

    Для растягивания сайта по вертикали

--- resources/views/site/universal2/legal.blade.php
+++ resources/views/site/universal2/legal.blade.php
@@ -1,6 +1,27 @@
 @extends('site.universal2.layout')
 
 @section('content')
+    <style>
+        body, .body-wrapper {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-content: space-between;
+        }
+
+        header {
+            flex-grow: 0;
+        }
+
+        .content {
+            flex-grow: 2;
+        }
+
+        footer {
+            flex-grow: 0;
+        }
+    </style>
+
     <div class="info-page-markdown content">
         {!! Markdown::parse($dictionary['legal_content']) !!}
     </div>
