08e4566e6875d2305eb74848689c00527d2d0d52
Марат Самиков <m.samikov@cdek.ru>
2022-07-26 20:15:38 +0700

    og:image для фейсбука

--- resources/views/site/universal2/layout.blade.php
+++ resources/views/site/universal2/layout.blade.php
@@ -13,6 +13,11 @@
     @endif
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+    <meta property="og:image" content="/request/images/index/poster-mobile-2x.jpg">
+    <meta property="og:image:width" content="640">
+    <meta property="og:image:height" content="664">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
