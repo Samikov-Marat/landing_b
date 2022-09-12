96de0c3d13c8837d251d2f38629272433a662145
Марат Самиков <m.samikov@cdek.ru>
2022-09-12 11:42:28 +0700

    MRKT-596 Поле Имя

--- app/Classes/Site/Jira/JiraSender.php
+++ app/Classes/Site/Jira/JiraSender.php
@@ -19,6 +19,7 @@ class JiraSender
             'issuetype' => ['id' => 15807],
             'priority' => ['name' => 'Высокий'],
             'summary' => trim($request->input('summary', 'Не указано')),
+            'customfield_15747' => $request->input('name', 'Не указано'),
             'customfield_10043' => $request->input('invoice_number', 'Не указано'),
             'customfield_13724' => $request->input('email', 'Не указано'),
             'description' => 'Телефон: ' . $request->input('phone', 'Не указано') .
