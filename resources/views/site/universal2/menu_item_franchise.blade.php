3c001294709531159d81801a462acbae2e479282
Швецов Сергей <s.shvetsov@cdek.ru>
2023-02-03 14:45:00 +0700

    MRKT-692 ссылка в меню на страницы франшизы

--- resources/views/site/universal2/menu_item_franchise.blade.php
+++ resources/views/site/universal2/menu_item_franchise.blade.php
@@ -1,5 +1,11 @@
 <li class="main-menu__item">
-    <a class="main-menu__link" href="{!! route('site.show_page', ['languageUrl' => $language->uri, 'pageUrl' => 'franchise']) !!}">
-        {{ $dictionary['menu_franchise'] }}
-    </a>
+    @if (isset($dictionary['menu_franchise_link']) and $dictionary['menu_franchise_link'])
+        <a class="main-menu__link" href="{!! $dictionary['menu_franchise_link'] !!}" target="_blank">
+            {{ $dictionary['menu_franchise'] }}
+        </a>
+    @else
+        <a class="main-menu__link" href="{!! route('site.show_page', ['languageUrl' => $language->uri, 'pageUrl' => 'franchise']) !!}">
+            {{ $dictionary['menu_franchise'] }}
+        </a>
+    @endif
 </li>
