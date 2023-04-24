becfa00e373c5b4c02cac0b8d127cc6689fe67e4
Марат Самиков <m.samikov@cdek.ru>
2023-04-24 21:33:32 +0700

    отладка

--- app/Classes/Site/SupportEmail.php
+++ app/Classes/Site/SupportEmail.php
@@ -4,15 +4,22 @@ namespace App\Classes\Site;
 
 use App\Mail\site\support\ForwardMail;
 use App\Mail\site\support\ShoppingMail;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SupportEmail
 {
     public static function sendShopping(Request $request)
     {
-        Mail::to(config('support.shopping_email'))
-            ->send(new ShoppingMail($request));
+        try {
+            Mail::to(config('support.shopping_email'))
+                ->send(new ShoppingMail($request));
+
+        }catch (Exception $e){
+            Log::error($e);
+        }
     }
 
     public static function sendForward(Request $request)
