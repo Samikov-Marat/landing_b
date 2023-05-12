3fdb5316e79493063161c7632e8dd06050f9311d
Швецов Сергей Олегович <s.shvetsov@cdek.ru>
2023-05-12 17:46:12 +0700

    MRKT-811 добавлены блоки с названиями тарифов

--- resources/views/site/universal2/documents.blade.php
+++ resources/views/site/universal2/documents.blade.php
@@ -11,14 +11,24 @@
                 </div>
                 <div class="two-icons docs-page__company-adv-icons">
                     <div class="two-icons__item two-icons__item_docs">
-                        <div class="two-icons__icon two-icons__icon_letter"></div>
-                        <div class="two-icons__text"><span class="two-icons__text-big">@d('documents_onpagetext_10')</span> @d('documents_onpagetext_11') <span class="two-icons__text-big">@d('documents_onpagetext_12')</span></div>
+                        <div class="two-icons__header">
+                            @d('document_first_tariff_header')
+                        </div>
+                        <div class="two-icons__details">
+                            <div class="two-icons__icon two-icons__icon_letter"></div>
+                            <div class="two-icons__text"><span class="two-icons__text-big">@d('documents_onpagetext_10')</span> @d('documents_onpagetext_11') <span class="two-icons__text-big">@d('documents_onpagetext_12')</span></div>
+                        </div>
                     </div>
 
                     @if (!empty($dictionary['documents_second_tariff_price']))
                         <div class="two-icons__item two-icons__item_docs">
-                            <div class="two-icons__icon two-icons__icon_letter"></div>
-                            <div class="two-icons__text"><span class="two-icons__text-big">@d('documents_second_tariff_weight')</span> @d('documents_onpagetext_11') <span class="two-icons__text-big">@d('documents_second_tariff_price')</span></div>
+                            <div class="two-icons__header">
+                                @d('document_second_tariff_header')
+                            </div>
+                            <div class="two-icons__details">
+                                <div class="two-icons__icon two-icons__icon_letter"></div>
+                                <div class="two-icons__text"><span class="two-icons__text-big">@d('documents_second_tariff_weight')</span> @d('documents_onpagetext_11') <span class="two-icons__text-big">@d('documents_second_tariff_price')</span></div>
+                            </div>
                         </div>
                     @endif
                 </div>
