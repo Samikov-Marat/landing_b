45cdaead3ae6dfd3b828af0134958d93504328ba
Швецов Сергей Олегович <s.shvetsov@cdek.ru>
2023-05-24 17:47:31 +0700

    MRKT-832 поправил логику валидации массивов

--- app/Http/Requests/LocalOfficeRequest.php
+++ app/Http/Requests/LocalOfficeRequest.php
@@ -34,22 +34,26 @@ class LocalOfficeRequest extends FormRequest
             'worktime.*' => 'string',
 
             'phone_old' => 'array',
-            'phone_old.phone_text' => 'string',
-            'phone_old.phone_value' => 'string',
-            'phone_old.show_at_footer' => 'string|integer|boolean',
+            'phone_old.*' => 'array:phone_text,phone_value,show_at_footer',
+            'phone_old.*.phone_text' => 'string',
+            'phone_old.*.phone_value' => 'string',
+            'phone_old.*.show_at_footer' => 'string|integer|boolean',
 
             'phone_new' => 'array',
-            'phone_new.phone_text' => 'string',
-            'phone_new.phone_value' => 'string',
-            'phone_new.show_at_footer' => 'string|integer|boolean',
+            'phone_new.*' => 'array:phone_text,phone_value,show_at_footer',
+            'phone_new.*.phone_text' => 'string',
+            'phone_new.*.phone_value' => 'string',
+            'phone_new.*.show_at_footer' => 'string|integer|boolean',
 
             'email_old' => 'array',
-            'email_old.email' => 'string',
-            'email_old.show_at_footer' => 'string|integer|boolean',
+            'email_old.*' => 'array:email,show_at_footer',
+            'email_old.*.email' => 'string',
+            'email_old.*.show_at_footer' => 'string|integer|boolean',
 
             'email_new' => 'array',
-            'email_new.email' => 'string',
-            'email_new.show_at_footer' => 'string|integer|boolean',
+            'email_new.*' => 'array:email,show_at_footer',
+            'email_new.*.email' => 'string',
+            'email_new.*.show_at_footer' => 'string|integer|boolean',
         ];
     }
 }
