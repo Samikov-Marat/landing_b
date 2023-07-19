58d2c1d8bf9bbb11c6e9d3a6fa0e35be2cd480db
Швецов Сергей Олегович <s.shvetsov@cdek.ru>
2023-07-19 14:46:27 +0700

    MRKT-893 добавлены разные урлы калькулятора для разных сред

--- config/calculator.php
+++ config/calculator.php
@@ -1,7 +1,13 @@
 <?php
 return [
-    'city_url' => 'http://qa2.locality.service.cdek.tech:8909/api/locality/international/autocomplete/city',
-    'url' => 'http://qa2.calculator.service.cdek.tech:8024/api/calculator/getServices',
+    'city_url' =>
+        env('APP_ENV', 'local') == 'production'
+            ? 'http://production.locality.service.cdek.tech:8909/api/locality/international/autocomplete/city'
+            : 'http://qa2.locality.service.cdek.tech:8909/api/locality/international/autocomplete/city',
+    'url' =>
+        env('APP_ENV', 'local') == 'production'
+            ? 'http://172.16.184.153:8024/api/calculator/getServices'
+            : 'http://qa2.calculator.service.cdek.tech:8024/api/calculator/getServices',
     // Универсальный договор без привилегий
     'sender_contract_id' => '533ac9a1-5743-4805-b635-95d0ff324c1e',
     // Бангладеш для b2b
