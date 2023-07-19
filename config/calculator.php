149bc8fb75adc37153860e057d8530b108fe391d
Швецов Сергей Олегович <s.shvetsov@cdek.ru>
2023-07-19 16:57:55 +0700

    MRKT-893 вынесены настройки калькулятора в env

--- config/calculator.php
+++ config/calculator.php
@@ -1,18 +1,9 @@
 <?php
 return [
-    'city_url' =>
-        env('APP_ENV', 'local') == 'production'
-            ? 'http://production.locality.service.cdek.tech:8909/api/locality/international/autocomplete/city'
-            : 'http://qa2.locality.service.cdek.tech:8909/api/locality/international/autocomplete/city',
-    'url' =>
-        env('APP_ENV', 'local') == 'production'
-            ? 'http://172.16.184.153:8024/api/calculator/getServices'
-            : 'http://qa2.calculator.service.cdek.tech:8024/api/calculator/getServices',
+    'city_url' => env('CITY_AUTOCOMPLETE', 'http://production.locality.service.cdek.tech:8909/api/locality/international/autocomplete/city'),
+    'url' => env('CALCULATOR_URL', 'http://172.16.184.153:8024/api/calculator/getServices'),
     // Универсальный договор без привилегий
-    'sender_contract_id' =>
-        env('APP_ENV', 'local') == 'production'
-        ? '533ac9a1-5743-4805-b635-95d0ff324c1e'
-        : '',
+    'sender_contract_id' => env('CALCULATOR_UNIVERSAL_CONTRACT_ID', ''),
     'special_sender_contracts' => [
         'cdek-bd.com' => '977d7aaf-ec3e-467d-a6e3-abeb10fd44cd'
     ]
