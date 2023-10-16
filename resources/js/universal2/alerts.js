463d96c0dc470040d6acc13d13cd148959719e53
Марат Самиков <m.samikov@cdek.ru>
2023-10-16 19:46:50 +0700

    MRKT-1160 Доработка розового алерта

--- resources/js/universal2/alerts.js
+++ resources/js/universal2/alerts.js
@@ -1,7 +1,23 @@
-$('.alert__personal-information').on('click', () => {
-  const faq_tab = $('.tab-alert__personal-information');
-  if (!faq_tab.hasClass('faq-list__faq_opened')) {
-    faq_tab.addClass('faq-list__faq_opened');
-  }
-  faq_tab.get(0).scrollIntoView();
-})
+$(function () {
+    $('.alert__personal-information').on('click', function () {
+        const faq_tab = $('.tab-alert__personal-information');
+        if (!faq_tab.length) {
+            location.href = $(this).data('href');
+            return false;
+        }
+        if (!faq_tab.hasClass('faq-list__faq_opened')) {
+            faq_tab.addClass('faq-list__faq_opened');
+        }
+        faq_tab.get(0).scrollIntoView();
+    })
+});
+
+$(function () {
+    if ('#personal-information' == location.hash) {
+        const faq_tab = $('.tab-alert__personal-information');
+        if (!faq_tab.hasClass('faq-list__faq_opened')) {
+            faq_tab.addClass('faq-list__faq_opened');
+        }
+        faq_tab.get(0).scrollIntoView();
+    }
+});
--- resources/views/site/universal2/alert_personal_information.blade.php
+++ resources/views/site/universal2/alert_personal_information.blade.php
@@ -1,6 +1,6 @@
 @if(!empty($dictionary['alert_personal_information']))
     <div class="alert__container">
-        <div class="alert alert__personal-information">
+        <div class="alert alert__personal-information" data-href="{!! route('site.show_page', ['languageUrl' => $language->uri, '#personal-information']) !!}">
             @d('alert_personal_information')
         </div>
     </div>
