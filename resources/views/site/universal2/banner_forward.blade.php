9635197428461bca243a3be0b47880d2a07a5e25
Марат Самиков <m.samikov@cdek.ru>
2023-10-30 15:34:37 +0700

    MRKT-1168 Блок с баннером Forward

---
+++ resources/views/site/universal2/banner_forward.blade.php
@@ -0,0 +1,7 @@
+<h2>@d('banner_forward_header')</h2>
+
+<div>@d('banner_forward_text')</div>
+
+<form method="get" action="{!! $dictionary['banner_forward_href'] !!}">
+    <button>@d('banner_forward_button')</button>
+</form>
