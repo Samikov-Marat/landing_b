d910eadec1175231096b2597d2f69679e6225a4a
Марат Самиков <m.samikov@cdek.ru>
2023-12-12 21:49:54 +0700

    Договор Вьетнама

--- config/calculator.php
+++ config/calculator.php
@@ -5,6 +5,7 @@ return [
     // Универсальный договор без привилегий
     'sender_contract_id' => env('CALCULATOR_UNIVERSAL_CONTRACT_ID', '533ac9a1-5743-4805-b635-95d0ff324c1e'),
     'special_sender_contracts' => [
-        'cdek-bd.com' => '977d7aaf-ec3e-467d-a6e3-abeb10fd44cd'
+        'cdek-bd.com' => '977d7aaf-ec3e-467d-a6e3-abeb10fd44cd',
+        'cdek.vn' => '14c0af99-f005-4345-a624-03e8f924a3e8',
     ]
 ];
